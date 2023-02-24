49a50450321f0af750b0435863e530c87ab6fdb2
Hedreon <hedreon@e.email>
2023-02-24 01:52:05 +0000

    the biggest visual change to the website ever

--- src/stars.js
+++ src/stars.js
@@ -1,14 +1,49 @@
 // CONFIGURABLE SETTINGS
-const numberOfStars = 1200;
-const fieldClass = ".starfield";
+const starAmount = 250; // Amount of stars to be displayed
+const elementName = ".stars"; // The name of the element for the stars to be grouped in
+const minDelay = 0; // Minimum delay in milliseconds
+const maxDelay = 5000; // Maximum delay in milliseconds
+const gridSize = 20; // The size of the grid in cells
 
-// DON'T MODIFY BELOW
-const backgroundClass = document.querySelector(fieldClass);
+// DO NOT MODIFY BELOW
+const groupClass = document.querySelector(elementName);
+const cellSize = 100 / gridSize;
+const usedCells = new Set();
 
-for (let i = 0; i < numberOfStars; i++) {
+function fadeStar(star) {
+    const randomDelay = Math.floor(Math.random() * 5000);
+    const fadeOutDuration = Math.floor(Math.random() * 8000);
+
+    setTimeout(() => {
+        if (star.style.opacity !== "0") {
+            star.style.opacity = "0";
+            setTimeout(() => {
+                star.style.opacity = "1";
+                fadeStar(star);
+            }, fadeOutDuration);
+        }
+    }, randomDelay);
+}
+
+for (let i = 0; i < starAmount; i++) {
+    let cellX = Math.floor(Math.random() * gridSize);
+    let cellY = Math.floor(Math.random() * gridSize);
+    
+    while (usedCells.has(`${cellX},${cellY}`)) {
+        cellX = (cellX + 1) % gridSize;
+        if (cellX === 0) {
+            cellY = (cellY + 1) % gridSize;
+        }
+    }
+
+    usedCells.add(`${cellX},${cellY}`);
+
     const newStar = document.createElement("div");
     newStar.classList.add("star");
-    newStar.style.top = `${Math.floor(Math.random() * 500)}vh`;
-    newStar.style.left = `${Math.floor(Math.random() * 98)}vw`;
-    backgroundClass.appendChild(newStar);
+    newStar.style.top = `${cellY * cellSize + Math.floor(Math.random() * cellSize)}%`;
+    newStar.style.left = `${cellX * cellSize + Math.floor(Math.random() * cellSize)}%`;
+
+    groupClass.appendChild(newStar);
+
+    fadeStar(newStar);
 }
